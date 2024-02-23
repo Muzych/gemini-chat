1edd0fe5ecd4c84b404720e2c2be97c3074a2a8b
Li0329 <haxk1024@gmail.com>
2024-02-23 18:33:01 +0800

    feat: update readme.md

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,10 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Button } from "@/components/ui/button";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Gemini-Chat" },
+    { name: "description", content: "Personal Assistant powerd by Gemini Pro" },
   ];
 };
 
