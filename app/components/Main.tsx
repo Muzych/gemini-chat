5dfcb17280f6385fe6a22276aa9264a67f4f8a81
Li0329 <haxk1024@gmail.com>
2024-03-15 22:27:18 +0800

    update main ui

--- app/components/Main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Main = () => {
-    return (
-        <div className='flex-grow'>Main</div>
-    )
-}
-
-export default Main
---
+++ app/components/main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className='flex-1 relative'>
+            <main className='overflow-y-auto w-full h-full'>
+                你好
+            </main>
+        </div>  
+    )
+}
+
+export default Main
